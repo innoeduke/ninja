388e6d54458b320d7e3cacab23fe942bfb3fae55
PGCaptain <kylewei@gmail.com>
2023-05-05 21:16:59 +0800

    Understand Components & Templates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import Navbar from "./Navbar";
 function App() {
+  const title = "Welcome to the Ninja blog!";
+  const numList = [1, 2, 3, 4, 5];
+
+  // Template part of the Component. First design it, and then write scripts up above.
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Navbar />
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Give me a random number: {Math.random()}</p>
+        <p>Show me a list: {numList}</p>
+        <p>
+          Add a URL for jump: <a href="http://www.moleqode.com">Moleqode.AI</a>
+        </p>
+      </div>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return ( 
+        <div className="navbar">
+            <h1>My Blog</h1>
+        </div>
+     );
+}
+ 
+export default Navbar;
