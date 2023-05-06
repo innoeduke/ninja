4a28ccc3c95181dca1576627e03abe18160a5c5f
PGCaptain <kylewei@gmail.com>
2023-05-06 10:03:39 +0800

    Learnt using map to create a list

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,9 @@
-import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-  const [index, setIndex] = useState(0);
-  // let index = 0;
-  let playerName = ["Zelda", "Link"][index];
-
-  const handleClick = (e) => {
-    // when index is set, the component will be re-rendered and the playerName will be updated
-    setIndex(1 - index);
-    // index = 1 - index;
-    console.log("Index: " + index + " => Hello " + playerName + "!", e);
-  };
-
-  const age = 100;
-
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <br />
-      <p>
-        {playerName} is {age} years old.
-      </p>
-      <button onClick={handleClick}>Click Me</button>
+      <BlogList />
     </div>
   );
 };
