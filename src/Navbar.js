085d7186531987c3bf620a5f7ef2426da939f7a8
PGCaptain <kylewei@gmail.com>
2023-05-07 00:48:12 +0800

    Learn to use Switch, Route and Link components

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,19 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <div className="navbar">
       <h1>Ninja Blog</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a
-          href="/create"
-          style={{
-            backgroundColor: "#f1356d",
-            color: "white",
-            borderRadius: "8px",
-          }}
-        >
-          New Blog
-        </a>
+        <Link to="/">Home</Link>
+        <Link to="/create">New Blog</Link>
       </div>
     </div>
   );
--- src/useFetch.js
+++ src/useFetch.js
@@ -6,25 +6,25 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    setTimeout(() => {
-      fetch(url)
-        .then((res) => {
-          if (!res.ok) {
-            throw Error("could not fetch the data for that resource");
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setData(data);
-          setIsLoading(false);
-          setError(null);
-        })
-        .catch((err) => {
-          console.log(err.message);
-          setError(err.message);
-          setIsLoading(false);
-        });
-    }, 500);
+    // setTimeout(() => {
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("could not fetch the data for that resource");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        console.log(err.message);
+        setError(err.message);
+        setIsLoading(false);
+      });
+    // }, 500);
   }, [url]);
   return { data, isLoading, error };
 };
