416aa80a396913517068362827ddc1cb668dd6b1
PGCaptain <kylewei@gmail.com>
2023-05-06 21:41:08 +0800

    fetch data from api endpoint (use json-server to simulate the api endpoint)

--- src/Home.js
+++ src/Home.js
@@ -2,32 +2,34 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
-
-  // const [count, setCount] = useState(0);
+  const [blogs, setBlogs] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   function handleDelete(id) {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
-    // if (id !== 2) setCount(count + 1);
   }
-
+  // use .then() to wait for the command to finish
   useEffect(() => {
-    console.log("@Home: use effect run ");
+    // setTimeout(() => {
+    fetch("http://localhost:6001/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+        setIsLoading(false);
+      });
+    // }, 3000);
   }, []);
 
+  // check blogs is not null and then render the BlogList component
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
+      {isLoading && <div>Loading...</div>}
+      {blogs && (
+        <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
+      )}
     </div>
   );
 };
